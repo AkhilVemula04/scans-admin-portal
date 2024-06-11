c681482e0a8420725ba58d401d43f095cc8edda2
AkhilVemula04 <akhilvemula00@gmail.com>
2024-06-11 09:06:43 -0700

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import '../App.css';
+import '../App.css'; 
 
 function Navbar() {
   return (
     <>
-      <h1 style={{ fontSize: '50px', color: 'red' }}>Navbar</h1>
-      <p className="para">This is a Navbar</p>
-      <h1 className="text-center text-4xl text-red-700">Navbar</h1>
-      <p className="para">This is a Navbar</p>
+      <nav className='bg-orange-800 h-13 flex items-center justify-between p-3'>
+        <div className='flex items-center'>
+         
+        </div>
+        <div className='space-x-4 text-white text-right'>
+          <a href='/Menu' className='text-white'>Menu</a> 
+          <a href='/Account' className='text-white'>Account</a>
+          <a href='/Settings' className='text-white'>Settings</a>
+          <a href='/Help' className='text-white'>Help</a>
+        </div>
+      </nav>
     </>
   );
 }
 
 export default Navbar;
+
